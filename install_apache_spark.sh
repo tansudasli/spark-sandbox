b4623055a07f7f42fe1434eb89019b10182569e7
Tansu Dasli <tansudasli@gmail.com>
2019-04-05 14:23:20 +0300

    spark installation script automated

--- install_apache_spark.sh
+++ install_apache_spark.sh
@@ -1,5 +1,25 @@
 #install java8
+sudo apt update && sudo apt -y upgrade
+sudo apt install -y openjdk-8-jdk
 
 #install python
+sudo apt install -y python3
+sudo apt install -y python3-pip
 
-#install spark
+#install spark 2.4 for linux
+wget http://archive.apache.org/dist/spark/spark-2.4.0/spark-2.4.0-bin-hadoop2.7.tgz
+tar -xvzf spark-2.4.0-bin-hadoop2.7.tgz
+
+#if u instal python3x, spark seeks for python command. so we should define alias.
+
+echo 'export PYSPARK_PYTHON=python3' >> .profile
+source .profile
+echo 'export PATH=$PATH:~/spark-2.4.0-bin-hadoop2.7/bin' >> .profile
+
+#test pyspark
+pyspark
+#define in the shell, sc.version
+
+#test scala-shell
+spark-shell
+#define in the shell, sc.version
