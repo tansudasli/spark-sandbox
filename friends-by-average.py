4c8b0ae2ec56095fea460183a713ba60dd1f259f
Tansu Dasli <tansudasli@gmail.com>
2019-04-06 16:11:51 +0300

    average by age completed

---
+++ friends-by-average.py
@@ -0,0 +1,25 @@
+# in the pyspark interactive shell (sc is automatically created)
+from pyspark import SparkConf, SparkContext
+
+
+# you don't need below 2 lines in pyspark interactive shell !
+conf = SparkConf().setMaster("local").setAppName("FriendByAverage")
+sc = SparkContext(conf=conf)
+
+line = sc.textFile("./spark-sandbox/fake-friends/friends.txt")
+
+# read file test
+line.count()
+
+f = line.map(
+    lambda line: line.split(",")).map(
+        lambda x: (x[2], int(x[3]))).mapValues(
+            lambda x: (x, 1)).reduceByKey(
+                lambda x, y: (x[0]+y[0], x[1]+y[1]))
+
+# test the result
+f.collect()
+
+average_by_age = f.mapValues(lambda x: x[0]/x[1])
+
+average_by_age.collect()
