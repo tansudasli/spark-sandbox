92f55762a9ebe9888ea251e40d5e5ae35a79ed07
Tansu Dasli <tansudasli@gmail.com>
2019-04-07 11:03:48 +0300

    white space cleaning

--- max-weather-on-year-1800.py
+++ max-weather-on-year-1800.py
@@ -10,9 +10,9 @@ lines = sc.textFile("./spark-sandbox/weather/year-1800.txt")  #['ITE00100554,180
 weathers = lines.map(
     lambda line: line.split(',')).map(                        #[['ITE00100554', '18000101', 'TMAX', '-75', '', '', 'E', ''], ['ITE00100554', '18000101', 'TMIN', '-148', '', '', 'E', '']]
         lambda x: (x[0], x[2], int(x[3]))).filter(            #[('ITE00100554', 'TMAX', '-75'), ('ITE00100554', 'TMIN', '-148')]
-            lambda x: "TMAX" in x[1]).map(                    #decrease 1825 lines to 730 !
-                lambda x: (x[0], x[2])).reduceByKey(          #[('ITE00100554', -148), ('EZE00100082', -135)]
-                    lambda x,y: max(x,y)).collect()           #result-> [('ITE00100554', -148), ('EZE00100082', -135)]
+            lambda x: "TMAX" in x[1]).map(
+                lambda x: (x[0], x[2])).reduceByKey(
+                    lambda x,y: max(x,y)).collect()
 
 for weather in weathers:
     print(weather[0] + "\t" + str(weather[1]))
