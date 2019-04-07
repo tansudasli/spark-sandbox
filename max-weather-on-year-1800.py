67e34a5e42ec35836eca8b4508ae60afabfb6650
Tansu Dasli <tansudasli@gmail.com>
2019-04-07 11:13:34 +0300

    data corrected

--- max-weather-on-year-1800.py
+++ max-weather-on-year-1800.py
@@ -9,7 +9,7 @@ sc = SparkContext(conf = conf)
 lines = sc.textFile("./spark-sandbox/weather/year-1800.txt")  #['ITE00100554,18000101,TMAX,-75,,,E,', 'ITE00100554,18000101,TMIN,-148,,,E,']
 weathers = lines.map(
     lambda line: line.split(',')).map(                        #[['ITE00100554', '18000101', 'TMAX', '-75', '', '', 'E', ''], ['ITE00100554', '18000101', 'TMIN', '-148', '', '', 'E', '']]
-        lambda x: (x[0], x[2], int(x[3]))).filter(            #[('ITE00100554', 'TMAX', '-75'), ('ITE00100554', 'TMIN', '-148')]
+        lambda x: (x[0], x[2], float(x[3])/10)).filter(         #[('ITE00100554', 'TMAX', '-75'), ('ITE00100554', 'TMIN', '-148')]
             lambda x: "TMAX" in x[1]).map(
                 lambda x: (x[0], x[2])).reduceByKey(
                     lambda x,y: max(x,y)).collect()
--- min-weather-on-year-1800.py
+++ min-weather-on-year-1800.py
@@ -9,7 +9,7 @@ sc = SparkContext(conf = conf)
 lines = sc.textFile("./spark-sandbox/weather/year-1800.txt")  #['ITE00100554,18000101,TMAX,-75,,,E,', 'ITE00100554,18000101,TMIN,-148,,,E,']
 weathers = lines.map(
     lambda line: line.split(',')).map(                        #[['ITE00100554', '18000101', 'TMAX', '-75', '', '', 'E', ''], ['ITE00100554', '18000101', 'TMIN', '-148', '', '', 'E', '']]
-        lambda x: (x[0], x[2], int(x[3]))).filter(            #[('ITE00100554', 'TMAX', '-75'), ('ITE00100554', 'TMIN', '-148')]
+        lambda x: (x[0], x[2], float(x[3]/10))).filter(       #[('ITE00100554', 'TMAX', '-75'), ('ITE00100554', 'TMIN', '-148')]
             lambda x: "TMIN" in x[1]).map(                    #decrease 1825 lines to 730 !
                 lambda x: (x[0], x[2])).reduceByKey(          #[('ITE00100554', -148), ('EZE00100082', -135)]
                     lambda x,y: min(x,y)).collect()           #result-> [('ITE00100554', -148), ('EZE00100082', -135)]
