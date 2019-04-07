ac089ea6fadbc07e65a838c1c10271bdf8394a0a
Tansu Dasli <tansudasli@gmail.com>
2019-04-07 10:39:02 +0300

    min weather on year 1800 calculated

--- min-weather-on-year-1800.py
+++ min-weather-on-year-1800.py
@@ -6,6 +6,13 @@ conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
 sc = SparkContext(conf = conf)
 
 #stationid,date,entryType,celsius,,,,
-lines = sc.textFile("./spark-sandbox/weather/year-1800.txt")
-weathers = lines.map(lambda line: line.split(',')).map(
-    lambda x: (x[0], x[2], x[3]))
+lines = sc.textFile("./spark-sandbox/weather/year-1800.txt")  #['ITE00100554,18000101,TMAX,-75,,,E,', 'ITE00100554,18000101,TMIN,-148,,,E,']
+weathers = lines.map(
+    lambda line: line.split(',')).map(                        #[['ITE00100554', '18000101', 'TMAX', '-75', '', '', 'E', ''], ['ITE00100554', '18000101', 'TMIN', '-148', '', '', 'E', '']]
+        lambda x: (x[0], x[2], int(x[3]))).filter(            #[('ITE00100554', 'TMAX', '-75'), ('ITE00100554', 'TMIN', '-148')]
+            lambda x: "TMIN" in x[1]).map(                    #decrease 1825 lines to 730 !
+                lambda x: (x[0], x[2])).reduceByKey(          #[('ITE00100554', -148), ('EZE00100082', -135)]
+                    lambda x,y: min(x,y))                     #result-> [('ITE00100554', -148), ('EZE00100082', -135)]
+
+for weather in weathers:
+    print(weather[0] + "\t" + str(weather[1]))
