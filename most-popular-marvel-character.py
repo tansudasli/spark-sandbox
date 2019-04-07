982b431470962c59b46a52738c46dc3ae93f498e
Tansu Dasli <tansudasli@gmail.com>
2019-04-07 21:11:17 +0300

    most popular marvel heros calculated

---
+++ most-popular-marvel-character.py
@@ -0,0 +1,33 @@
+from pyspark import SparkConf, SparkContext
+
+# you don't need below 2 lines in pyspark interactive shell !
+conf = SparkConf().setMaster("local").setAppName("MostPopularMarvelCharacter")
+sc = SparkContext(conf = conf)
+
+#broadcast marvel character names to all executers
+#marvel-hero-id hero-name
+def loadMarvelHeroNames():
+    marvelHeroNames = {}
+    f = open("./spark-sandbox/marvel/marvel-hero-names.txt", "r", encoding="utf-8", errors="ignore")
+    lines = f.readlines()
+    for line in lines:
+        fields = line.split(" ")
+        marvelHeroNames[int(fields[0])] = fields[1]
+    return marvelHeroNames
+
+#broadcast master dataset
+heroDictionary = sc.broadcast(loadMarvelHeroNames())
+
+#marvel-hero-id marvel-hero-id marvel-hero-id marvel-hero-id ....
+lines = sc.textFile("./spark-sandbox/marvel/marvel-graph.txt")
+movies = lines.flatMap(
+    lambda line: line.split()).map(
+        lambda heroID: (heroID, 1)).reduceByKey(
+            lambda x,y: (x+y)).map(
+                lambda x: (x[1],x[0])).sortByKey().map(
+                    lambda x: (heroDictionary.value[int(x[1])], x[0])).collect()
+
+
+for key, value in movies:
+    #movie-name popularity
+    print("%s %i" % (key, value))
