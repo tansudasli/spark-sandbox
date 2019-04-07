935acc64be14e52093b4676905581169349c7526
Tansu Dasli <tansudasli@gmail.com>
2019-04-07 19:51:11 +0300

    movienames broadcasted

--- most-popular-movie.py
+++ most-popular-movie.py
@@ -4,14 +4,29 @@ from pyspark import SparkConf, SparkContext
 conf = SparkConf().setMaster("local").setAppName("MostPopularMovie")
 sc = SparkContext(conf = conf)
 
+#broadcast movienames to all executers, for meaningful movie ids
+#movie-id|movie-name|movie-date|imdb-url|0|0|0|1|1|1|0|0|0|0|0|0|0|0|0|0|0|0|0
+def loadMovieNames():
+    movieNames = {}
+    f = open("./spark-sandbox/ml-100k/u.item", "r", encoding="utf-8", errors="ignore")
+    lines = f.readlines()
+    for line in lines:
+        fields = line.split("|")
+        movieNames[int(fields[0])] = fields[1]
+    return movieNames
+
+nameDictionary = sc.broadcast(loadMovieNames())
+
 #user-id,movie-id,rating,timespamp
 lines = sc.textFile("./spark-sandbox/ml-100k/u.data")
 movies = lines.map(
     lambda x: x.split()).map(
-        lambda x: (x[1], 1)).reduceByKey(
-            lambda x,y: (x+y)).map(lambda x: (x[1], x[0])).sortByKey().collect()
+        lambda x: (int(x[1]), 1)).reduceByKey(
+            lambda x,y: (x+y)).map(
+                lambda x: (x[1], x[0])).sortByKey().map(
+                    lambda x: (nameDictionary.value[x[1]], x[0])).collect()
 
 
 for key, value in movies:
     #movie-id popularity
-    print("%s %i" % (value, key))
+    print("%s %i" % (key, value))
