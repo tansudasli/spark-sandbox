57f24588c2226629640ee875fdd8f6dada203e3d
Tansu Dasli <tansudasli@gmail.com>
2019-04-07 17:51:17 +0300

    word count for removed unnecessary punctiations

--- word-count-enhanced.py
+++ word-count-enhanced.py
@@ -1,12 +1,17 @@
+import re
 from pyspark import SparkConf, SparkContext
-import collections
+
 
 # you don't need below 2 lines in pyspark interactive shell !
 conf = SparkConf().setMaster("local").setAppName("WordCounterEnhanced")
 sc = SparkContext(conf = conf)
 
 lines = sc.textFile("./spark-sandbox/words/book.txt")
-words = lines.flatMap(lambda lines: lines.split()).countByValue()
+
+words = lines.flatMap(
+    lambda lines: re.compile(r'\w+', re.UNICODE).split(lines.lower())).map(
+        lambda word: (word,1)).reduceByKey(
+            lambda x,y: x+y).collect()
             
 for word, count in words.items():
     print(word + "\t" + str(count))
